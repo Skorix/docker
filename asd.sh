c2ef10c2a69c7d8c085ad01fbfbefcf1ecbda7f5
Skorix <54755559+Skorix@users.noreply.github.com>
2026-02-21 15:19:31 +0300

    Update asd.sh

--- asd.sh
+++ asd.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Пробуем скачать официальный образ через работающее зеркало
-echo "Скачиваю официальный образ через зеркало..."
-docker pull huecker.io/moonlight-stream/moonlight-chrome-wasmv2:latest
+echo "Скачиваю образ через облачное зеркало Timeweb..."
+# Используем зеркало, которое работает в РФ без сбоев
+docker pull dockerhub.timeweb.cloud/moonlightstream/moonlight-chrome-wasmv2:latest
 
-# Создаем тег, чтобы compose его подхватил
-docker tag huecker.io/moonlight-stream/moonlight-chrome-wasmv2:latest ghcr.io/moonlight-stream/moonlight-chrome-wasmv2:latest
+echo "Создаю локальный тег..."
+# Переименовываем, чтобы docker-compose его "узнал"
+docker tag dockerhub.timeweb.cloud/moonlightstream/moonlight-chrome-wasmv2:latest ghcr.io/moonlight-stream/moonlight-chrome-wasmv2:latest
 
 echo "Готово! Запускай docker compose up -d"
